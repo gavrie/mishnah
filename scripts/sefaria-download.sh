ac00747de04337e925326f677ba9fb65705bd249
Gavrie Philipson <gavrie@philipson.co.il>
2017-12-31 08:20:46 +0200

    Initial

---
+++ scripts/sefaria-download.sh
@@ -0,0 +1,6 @@
+#svn export https://github.com/Sefaria/Sefaria-Export/trunk/json/Mishnah/Seder%20Zeraim
+svn export https://github.com/Sefaria/Sefaria-Export/trunk/json/Mishnah/Seder%20Moed
+svn export https://github.com/Sefaria/Sefaria-Export/trunk/json/Mishnah/Seder%20Nashim
+svn export https://github.com/Sefaria/Sefaria-Export/trunk/json/Mishnah/Seder%20Nezikin
+svn export https://github.com/Sefaria/Sefaria-Export/trunk/json/Mishnah/Seder%20Kodashim
+svn export https://github.com/Sefaria/Sefaria-Export/trunk/json/Mishnah/Seder%20Tahorot
